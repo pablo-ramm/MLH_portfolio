a5c21bcb189594b375cb56917e7df08b7780f988
pablo-ramm <a01702872@tec.mx>
2023-06-24 20:18:30 -0600

    Add jinja template

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/aboutMe')
+def aboutMe():
+    return render_template('aboutMe.html', title="About Me")
