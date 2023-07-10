372dc89f00bc066e6952e9f7c5f0371fdf0cc026
Alfonso Amaya <alfonsocamargo0108@gmail.com>
2023-07-09 20:08:13 -0600

    Add redeploy script to project

---
+++ redeploy-website.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmux kill-server
+cd ./MLH_portfolio_alfonso
+git fetch && git reset origin/main --hard
+pwd
+source python3-virtualenv/bin/activate && pip install -r requirements.txt && deactivate && echo "\n\n\n"
+#tmux new && source python3-virtualenv/bin/activate && flask run --host 0.0.0.0
+tmux new -s portfolio -d # Create a new session but dont attach to it
+tmux send-keys -t portfolio 'source python3-virtualenv/bin/activate' C-m
+tmux send-keys -t portfolio 'flask run --host 0.0.0.0' C-m
+tmux attach -t portfolio
