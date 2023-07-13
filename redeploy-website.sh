861f115c4fcba63f688d0fd21138f09605608ced
Alfonso Amaya <54002890+Alfonso-AmayaC@users.noreply.github.com>
2023-07-13 17:47:32 -0600

    Update redeploy-website.sh
    Change tmux statements for systemctl ones

--- redeploy-website.sh
+++ redeploy-website.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-tmux kill-server
-cd ./MLH_portfolio_alfonso
+systemctl stop myportfolio
+cd /home/MLH_portfolio_alfonso
 git fetch && git reset origin/main --hard
-pwd
-source python3-virtualenv/bin/activate && pip install -r requirements.txt && deactivate && echo "\n\n\n"
-tmux new -s portfolio -d # Create a new session but dont attach to it
-tmux send-keys -t portfolio 'source python3-virtualenv/bin/activate' C-m
-tmux send-keys -t portfolio 'flask run --host 0.0.0.0' C-m
-tmux attach -t portfolio
+source python3-virtualenv/bin/activate && pip install -r requirements.txt && deactivate
+systemctl restart myportfolio
