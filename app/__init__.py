19fc11dd252de53728147d3414058020e10b3606
pablo-ramm <a01702872@tec.mx>
2023-07-16 13:29:25 -0600

    add Timeline Page and db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,36 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
+from peewee import *
+from playhouse.shortcuts import model_to_dict
+import datetime
+import requests
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(
+    os.getenv('MYSQL_DATABASE'),
+    user=os.getenv('MYSQL_USER'),
+    password=os.getenv('MYSQL_PASSWORD'),
+    host=os.getenv('MYSQL_HOST'),
+    port=3306
+)
+
+
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    
+    class Meta:
+        database = mydb
+
+mydb.connect()
+print(mydb)
+mydb.create_tables([TimelinePost])
 
 hobbiesArray = [{
     "name": "Coding",
@@ -81,3 +108,29 @@ def education_page():
 @app.route('/experience')
 def experience_page():
     return render_template('experience.html', title='Work Experience', work_experience=work_experience)
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_post': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/timeline')
+def timeline():
+    response = requests.get('http://localhost:5000/api/timeline_post')
+    print(response.json()['timeline_post'])
+    posts = response.json()['timeline_post']
+    
+    return render_template('timeline.html', posts=posts)
